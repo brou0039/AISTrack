45a0b4165ce6f35f743fd1f97a0229e1f3023112
brou0039 <maaikebrwr@hotmail.com>
2015-12-11 10:32:35 +0100

    simple query to find out how the database has been built.

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
   //db.php is used to connect to the database. in this file a mysqli object is made, called $con.
 require_once 'db.php';
 
-$sql = "SELECT * FROM ais_data";
+$sql = "SELECT * FROM ais_data WHERE timestap = '2015-11-12 10:26:00'";
 $result = mysqli_query($con, $sql);
 print_r($result);
 ?>
+
