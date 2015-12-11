a4aaf9254ca47cbae9b9a3a1af78e068801c78de
brou0039 <maaikebrwr@hotmail.com>
2015-12-11 13:28:57 +0100

    Tabel met alle schepen. Werkt helaas nog niet zoals gewenst. Query geeft nu nog meerdere keren hetzelfde schip weer, wat in principe niet zou moeten.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <?php
 /**
  * Created by PhpStorm.
@@ -9,8 +10,44 @@
   //db.php is used to connect to the database. in this file a mysqli object is made, called $con.
 require_once 'db.php';
 
-$sql = "SELECT * FROM ais_data WHERE timestap = '2015-11-12 10:26:00'";
-$result = mysqli_query($con, $sql);
-print_r($result);
+$sql = "SELECT DISTINCT mmsi, name, timestamp FROM ais_data ORDER BY timestamp DESC";
+
 ?>
 
+<html>
+<head>
+    <title>Hoofdpagina PoC groep C</title>
+</head>
+<body>
+<h1>Hoofdpagina</h1>
+<h3>Op deze pagina vindt u een overzicht van alle schepen die de laatste tijd in de haven zijn geweest. Klik op één van
+de schepen om meer informatie te krijgen over dit schip.</h3>
+<table>
+    <tr>
+        <th>
+            MMSI nummer
+        </th>
+        <th>
+            Naam schip
+        </th>
+        <th>
+            Laatst geregistreerde aanwezigheid
+        </th>
+    </tr>
+
+<?php
+if($result = mysqli_query($con, $sql))
+{
+    while($row = mysqli_fetch_assoc($result))
+    {
+        echo '<tr><td>'. $row['mmsi'] . '</td><td>' . $row['name'] . '</td><td>' . $row['timestamp']. '</td></tr>';
+    }
+    mysqli_free_result($result);
+}
+mysqli_close($con);
+
+?>
+
+</table>
+</body>
+</html>
