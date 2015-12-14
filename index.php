d00c6e22850707f8d956fa98ac65fcabebfdfb73
rrooij <rderooij685@gmail.com>
2015-12-14 19:54:23 +0100

    index.php: use JQuery for GET request
    We include JQuery anyway, and doing plain XHTTP Requests is boilerplate
    code.

--- index.php
+++ index.php
@@ -18,27 +18,15 @@ require_once 'db.php';
     <link rel="stylesheet" href="style.css" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
-        function showShips(str) {
-            if (str == "") {
-                document.getElementById("txtHint").innerHTML = "";
-
-            } else {
-                if (window.XMLHttpRequest) {
-                    // code for IE7+, Firefox, Chrome, Opera, Safari
-                    xmlhttp = new XMLHttpRequest();
-                } else {
-                    // code for IE6, IE5
-                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                xmlhttp.onreadystatechange = function () {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
-                    }
-                };
-                xmlhttp.open("GET", "getships.php", true);
-                xmlhttp.send();
-            }
+        function showShips() {
+            $.get("getships.php", function(data) {
+                document.getElementById("txtHint").innerHTML = data;
+            });
         }
+
+    $(document).ready(function(){
+        $("#showShipButton").click(showShips);
+    });
     </script>
 
 </head>
@@ -46,7 +34,7 @@ require_once 'db.php';
 <h1>Hoofdpagina</h1>
 <h3>Op deze pagina vindt u een overzicht van alle schepen die de laatste tijd in de haven zijn geweest. Klik op één van
 de schepen om meer informatie te krijgen over dit schip.</h3>
-<button onclick="showShips(this.value)" value="gitm">Get Ships</button>
+<button id="showShipButton" value="gitm">Get Ships</button>
 <div id="txtHint"><b>Ship info will be listed here...</b></div>
 </body>
 </html>
