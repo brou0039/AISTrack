48268f67a446344c5aa0b0c68a6e1dc0f3e31734
rrooij <rderooij685@gmail.com>
2015-12-14 20:35:56 +0100

    track.php: add simple pagination
    The pagination was considered necessary because of the huge amount of
    records that a ship could return.
    
    I'm sorry for this dirty work, please note that this is a prove of
    concept and as such doesn't need proper code.

--- track.php
+++ track.php
@@ -21,7 +21,18 @@ require_once 'db.php';
 
 <?php
 if (isset($_GET['mmsi'])) {
-    $sql = "SELECT name, timestamp, longitude, latitude, navstat FROM ais_data WHERE mmsi=$_GET[mmsi] ORDER BY TIMESTAMP DESC";
+    $count = "SELECT count(mmsi) as count FROM ais_data WHERE mmsi=$_GET[mmsi]";
+    if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+    $num_rec_per_page = 50;
+    $total_records = 0;
+    $start_from = ($page-1) * $num_rec_per_page;
+    if ($result = mysqli_query($con, $count)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $total_records = $row['count'];
+        }
+    mysqli_free_result($result);
+}
+    $sql = "SELECT name, timestamp, longitude, latitude, navstat FROM ais_data WHERE mmsi=$_GET[mmsi] ORDER BY TIMESTAMP DESC LIMIT $start_from, $num_rec_per_page";
 
     if ($result = mysqli_query($con, $sql)) {
         echo '
@@ -52,8 +63,12 @@ if (isset($_GET['mmsi'])) {
             $date = DateTime::createFromFormat('U', $row['timestamp']);
             echo '<tr><td style="font-size: x-small">' . date_format($date, 'H:i:s d-m-Y') . '</td><td>' . $row['longitude'] . '</td><td>' . $row['latitude'] . '</td><td>' . $row['navstat'] . '</td><td><a href="https://www.google.nl/maps/@' . $row['latitude'] . ',' . $row['longitude'] . ',17z?hl=en">Google Maps</a></td></tr>';
         }
+        $total_pages = ceil($total_records / $num_rec_per_page);
         mysqli_free_result($result);
         echo '</table>';
+        for ($i=1; $i<=$total_pages; $i++) {
+            echo "<a href='track.php?mmsi=" . $_GET['mmsi'] . "&page=" . $i ."&name=" . $_GET['name'] . "'>".$i. "</a> ";
+        };
     }
 } else {
     echo "<h2>Oeps!</h2> er is iets fout gegaan. <br />Ga terug naar de hoofdpagina en probeer het opnieuw.<br/><br/>";
