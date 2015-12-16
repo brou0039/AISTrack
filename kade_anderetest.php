96aee7ad2bb3a118c69af53e30dbe4fc1c8ab5b3
Robin <robin007bond@gmail.com>
2015-12-16 12:23:40 +0100

    Make function for retrieving quay

--- kade_anderetest.php
+++ kade_anderetest.php
@@ -6,25 +6,28 @@
  * Time: 19:31
  */
 require_once 'db.php';
-require_once 'geoPHP/geoPHP.inc';
 
-$sql = 'SELECT K.id AS kid, K.naam AS knaam , P.id AS pid , P.longitude AS plongitude, P.latitude AS platitude
+function getKades()
+{
+    $sql = 'SELECT K.id AS kid, K.naam AS knaam , P.id AS pid , P.longitude AS plongitude, P.latitude AS platitude
         FROM kadepunt P , kade K WHERE K.id = P.Kade_id ORDER BY K.id ASC , P.id ASC';
-$kade = null;
-$kades = array();
+    $kade = null;
+    $kades = array();
 
-if ($result = mysqli_query($con, $sql)) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $kid = $row['kid'];
-        if (!isset($kades[$kid])) {
-            $kades[$row['kid']] = array();
+    if ($result = mysqli_query($con, $sql)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $kid = $row['kid'];
+            if (!isset($kades[$kid])) {
+                $kades[$row['kid']] = array();
+            }
+            $latitude = $row['platitude'];
+            $longitude = $row['plongitude'];
+            array_push($kades[$kid], array('latitude' => $latitude,
+                                          'longitude' => $longitude));
         }
-        $latitude = $row['platitude'];
-        $longitude = $row['plongitude'];
-        array_push($kades[$kid], array('latitude' => $latitude,
-                                       'longitude' => $longitude));
-        }
-    mysqli_free_result($result);
+        mysqli_free_result($result);
+        mysqli_close($con);
+        return kades;
+    }
+    return null;
 }
-var_dump($kades);
-mysqli_close($con);
